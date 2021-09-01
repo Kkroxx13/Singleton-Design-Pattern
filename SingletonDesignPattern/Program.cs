eff85107670b3724862e236b58768d64ff1f3b98
Karan Kanchan <karan.kanchan@morningstar.com>
2021-09-01 22:18:40 +0530

    Implemented Singleton Design Pattern

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton sl1 = Singleton.Instance();
+            Singleton sl2 = Singleton.Instance();
+            if(sl1==sl2)
+                Console.WriteLine("Both objects are same");
+            Console.ReadLine();
+                
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        public Singleton()
+        {
+
+        }
+        public static Singleton Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
